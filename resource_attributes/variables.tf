8b38a3b165ce30968f801831c49b4563c89374fc
Kemal Mutlu <mutlukemal1717@gmail.com>
2023-02-19 16:38:00 +0300

    resource attributes

---
+++ resource_attributes/variables.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+    default = "pets.txt"
+}
+
+variable "prefix" {
+    default = "Mrs"
+}
+
+variable "separator" {
+    default = "."
+}
+
+variable "length" {
+    default = "1"
+}

302d185a7ec9cd08cd7c56f2cb78721e5141eb88
Kemal Mutlu <mutlukemal1717@gmail.com>
2023-02-19 19:16:59 +0300

    output variables

---
+++ output_variables/variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+    default = "pets.txt"
+}
+
+variable "content" {
+    default = "I love pets!"
+}
+
+variable "prefix" {
+    default = "Mrs"
+}
+
+variable "separator" {
+    default = "."
+}
+
+variable "length" {
+    default = "1"
+}
